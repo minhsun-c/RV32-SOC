ce3ac1a802a617fa76d5ade9a383eb68e218437f
minhsun-c <107985606+minhsun-c@users.noreply.github.com>
2024-10-05 22:37:51 +0800

    Upload HW1

---
+++ HW1/src/include.v
@@ -0,0 +1,14 @@
+`ifndef __HEADERS
+`define __HEADERS
+
+`include "defines.v"
+`include "exe_mem.v"
+`include "id_exe.v"
+`include "id.v"
+`include "mem_wb.v"
+`include "mem.v"
+`include "pc_reg.v"
+`include "regfile.v"
+`include "rom.v"
+
+`endif 
